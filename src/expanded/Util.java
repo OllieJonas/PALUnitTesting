979e7d5447d63efc5ade383877af9a70b9e83b92
ollie <olliejonas@gmail.com>
2020-11-20 13:08:51 +0000

    Added private constructor to Util

--- src/expanded/Util.java
+++ src/expanded/Util.java
@@ -2,6 +2,9 @@ package expanded;
 
 public class Util {
 
+    private Util() {
+    }
+
     public static String listToStr(String[] str) {
         return "[" + String.join(", ", str) + "]";
     }
