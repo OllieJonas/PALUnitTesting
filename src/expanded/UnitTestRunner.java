e5de6a09f95d95db4b645337b137ef9b1ba4ac95
ollie <olliejonas@gmail.com>
2020-11-20 12:44:47 +0000

    More refactoring

--- src/expanded/UnitTestRunner.java
+++ src/expanded/UnitTestRunner.java
@@ -5,6 +5,29 @@ import java.lang.reflect.Method;
 
 public class UnitTestRunner {
 
+    private final TestSuite suite;
+
+    private final Method method;
+
+    public UnitTestRunner(TestSuite suite, Method method) {
+        this.suite = suite;
+        this.method = method;
+    }
+    /** Assumes we've already filtered the list of declared methods for the annotation {@link Test} */
+    public TestStatus run() {
+        try {
+            invoke(method);
+            return TestStatus.passed();
+        } catch (Exception e) {
+            return TestStatus.failed(e);
+        }
+    }
+
+    private void invoke(Method method) throws IllegalAccessException, InvocationTargetException {
+        method.setAccessible(true);
+        method.invoke(suite);
+    }
+
     public static class TestStatus {
 
         private final Exception thrown;
@@ -39,28 +62,5 @@ public class UnitTestRunner {
             PASSED,
             FAILED;
         }
-    }
-
-    private final TestSuite suite;
-
-    private final Method method;
-
-    public UnitTestRunner(TestSuite suite, Method method) {
-        this.suite = suite;
-        this.method = method;
-    }
-    /** Assumes we've already filtered the list of declared methods for the annotation {@link Test} */
-    public TestStatus run() {
-        try {
-            invoke(method);
-            return TestStatus.passed();
-        } catch (Exception e) {
-            return TestStatus.failed(e);
-        }
-    }
-
-    private void invoke(Method method) throws IllegalAccessException, InvocationTargetException {
-        method.setAccessible(true);
-        method.invoke(suite);
     }
 }
