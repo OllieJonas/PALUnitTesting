789efede728e00d2adb35fb4222e82ad066f6780
Ollie <olliejonas@gmail.com>
2020-11-21 11:59:18 +0000

    Brief commenting on @Test

--- src/expanded/testmeta/Test.java
+++ src/expanded/testmeta/Test.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * These variables are only used for nicer printing when logging all tests and the final breakdown report - they're by
+ * no means necessary.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Test {
